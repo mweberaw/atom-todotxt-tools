295c07484dfd6e6b63b99e4b9cbb418a059a31c7
Mathias Weber <mathias@weber-inter.net>
2016-05-26 21:11:10 +0200

    archive functionality and commit Javascript result

--- src/todotxt-tools.ts
+++ src/todotxt-tools.ts
@@ -1,4 +1,5 @@
 import {CompositeDisposable} from 'atom';
+import path = require('path')
 import {AutocompleteProvider} from './todotxt-autocompleteprovider';
 
 let provider: AutocompleteProvider = null;
@@ -10,6 +11,8 @@ export function activate() {
     'todotxt:add-todo', ()=> addTodo()));
   subscriptions.add(atom.commands.add('atom-text-editor',
     'todotxt:finish-todo', ()=> finishTodo()));
+  subscriptions.add(atom.commands.add('atom-text-editor',
+    'todotxt:archive', ()=> archive()));
 }
 
 export function deactivate() {
@@ -43,18 +46,47 @@ function finishTodo() {
   editor.insertText("x " + timestamp(today) + " ")
   editor.selectLinesContainingCursors()
   let doneItem = editor.getSelectedText()
-  editor.delete() // delete selection
-  editor.delete() // delete newline
+  editor.delete()
   editor.moveToBottom()
   if (editor.getCursorBufferPosition().column !== 0) {
     editor.insertNewline()
   }
   editor.insertText(doneItem)
-  editor.insertNewline()
+  editor.save()
 }
 
 function timestamp(date: Date) {
   return date.toISOString().substr(0, 10);
+}
+
+function archive() {
+  let todoEditor = atom.workspace.getActiveTextEditor()
+  let todoPath = todoEditor.getPath()
+  let basedir = path.dirname(todoPath)
+  atom.workspace.open(path.join(basedir, 'done.txt'))
+  .then((doneEditor: AtomCore.TextEditor) => {
+    doneEditor.moveToBottom()
+    if (doneEditor.getCursorBufferPosition().column !== 0) {
+      doneEditor.insertNewline()
+    }
+    todoEditor.moveToBottom()
+    if (todoEditor.getCursorBufferPosition().column !== 0) {
+      todoEditor.insertNewline()
+    }
+    todoEditor.moveToTop()
+    while (todoEditor.getCursorBufferPosition().row !== (todoEditor.getLineCount() - 1)) {
+      todoEditor.selectLinesContainingCursors()
+      let line = todoEditor.getSelectedText()
+      if (/^x /.test(line)) {
+        doneEditor.insertText(line)
+        todoEditor.delete()
+      } else {
+        todoEditor.moveDown()
+      }
+    }
+    todoEditor.save()
+    doneEditor.save()
+  })
 }
 
 export function provideAutocomplete() {
